0f5b933bdbaaa74a2d4ffc05c466fd45c3febbd4
kevin-ZHONG <kevin@kevin-ZHONGdeMacBook-Pro.local>
2016-09-29 18:06:08 +0800

    upload js

---
+++ gulpfile.js
@@ -0,0 +1,217 @@
+var gulp = require('gulp')
+var gutil = require('gulp-util')
+var uglify = require('gulp-uglify')
+var watchPath = require('gulp-watch-path')
+var combiner = require('stream-combiner2')
+var sourcemaps = require('gulp-sourcemaps')
+var minifycss = require('gulp-minify-css')
+var autoprefixer = require('gulp-autoprefixer')
+var sass = require('gulp-ruby-sass')
+var imagemin = require('gulp-imagemin')
+var handlebars = require('gulp-handlebars');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+var minifyHtml = require("gulp-minify-html");
+var clean = require('gulp-clean');
+
+var handleError = function (err) {
+    var colors = gutil.colors;
+    console.log('\n')
+    gutil.log(colors.red('Error!'))
+    gutil.log('fileName: ' + colors.red(err.fileName))
+    gutil.log('lineNumber: ' + colors.red(err.lineNumber))
+    gutil.log('message: ' + err.message)
+    gutil.log('plugin: ' + colors.yellow(err.plugin))
+}
+
+
+gulp.task('html',function () {
+    gulp.src('src/*.html') // 要压缩的html文件
+        .pipe(minifyHtml()) //压缩
+        .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('watchjs', function () {
+    gulp.watch('src/js/**/*.js', function (event) {
+        var paths = watchPath(event, 'src/', 'dist/')
+        gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+        gutil.log('Dist ' + paths.distPath)
+
+        var combined = combiner.obj([
+            gulp.src(paths.srcPath),
+            sourcemaps.init(),
+            uglify(),
+            sourcemaps.write('./'),
+            gulp.dest(paths.distDir)
+        ])
+
+        combined.on('error', handleError)
+    })
+})
+
+gulp.task('js', function () {
+    var combined = combiner.obj([
+        gulp.src('src/js/**/*.js'),
+        sourcemaps.init(),
+        uglify(),
+        sourcemaps.write('./'),
+        gulp.dest('dist/js/')
+    ])
+    combined.on('error', handleError)
+})
+
+
+gulp.task('watchcss',function () {
+    gulp.watch('src/css/**/*.css', function (event) {
+        var paths = watchPath(event, 'src/', 'dist/')
+
+        gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+        gutil.log('Dist ' + paths.distPath)
+
+        gulp.src(paths.srcPath)
+            .pipe(sourcemaps.init())
+            .pipe(autoprefixer({
+              browsers: 'last 2 versions'
+            }))
+            .pipe(minifycss())
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(paths.distDir))
+    })
+})
+
+gulp.task('css', function () {
+    gulp.src('src/css/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+          browsers: 'last 2 versions'
+        }))
+        .pipe(minifycss())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/css/'))
+})
+
+gulp.task('watchsass',function () {
+    gulp.watch('src/sass/**/*', function (event) {
+        var paths = watchPath(event, 'src/sass/', 'dist/css/')
+
+        gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+        gutil.log('Dist ' + paths.distPath)
+        sass(paths.srcPath)
+            .on('error', function (err) {
+                console.error('Error!', err.message);
+            })
+            .pipe(sourcemaps.init())
+            .pipe(minifycss())
+            .pipe(autoprefixer({
+              browsers: 'last 2 versions'
+            }))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(paths.distDir))
+    })
+})
+
+gulp.task('sass', function () {
+        sass('src/sass/')
+        .on('error', function (err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(sourcemaps.init())
+        .pipe(minifycss())
+        .pipe(autoprefixer({
+          browsers: 'last 2 versions'
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/css'))
+})
+
+
+gulp.task('watchimage', function () {
+    gulp.watch('src/images/**/*', function (event) {
+        var paths = watchPath(event,'src/images/','dist/images/')
+
+        gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+        gutil.log('Dist ' + paths.distPath)
+
+        gulp.src(paths.srcPath)
+            .pipe(imagemin({
+                progressive: true
+            }))
+            .pipe(gulp.dest(paths.distDir))
+    })
+})
+
+gulp.task('image', function () {
+    gulp.src('src/images/**/*')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('dist/images'))
+})
+
+
+
+gulp.task('watchtemplates', function () {
+    gulp.watch('src/templates/**/*', function (event) {
+        var paths = watchPath(event, 'src/', 'dist/')
+
+        gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+        gutil.log('Dist ' + paths.distPath)
+
+        var combined = combiner.obj([
+            gulp.src(paths.srcPath),
+            handlebars({
+              // 3.0.1
+              handlebars: require('handlebars')
+            }),
+            wrap('Handlebars.template(<%= contents %>)'),
+            declare({
+              namespace: 'S.templates',
+              noRedeclare: true
+            }),
+            gulp.dest(paths.distDir)
+        ])
+        combined.on('error', handleError)        
+    })
+})
+
+gulp.task('templates', function () {
+        gulp.src('src/templates/**/*')
+        .pipe(handlebars({
+          // 3.0.1
+          handlebars: require('handlebars')
+        }))
+        .pipe(wrap('Handlebars.template(<%= contents %>)'))
+        .pipe(declare({
+          namespace: 'S.templates',
+          noRedeclare: true
+        }))
+        .pipe(gulp.dest('dist/templates'))
+})
+
+
+gulp.task('clean', function() {
+     return gulp.src(['dist/*'])
+    .pipe(clean());
+});
+
+gulp.task('default', ['watchjs', 'watchcss', 'watchsass', 'watchimage', 'watchtemplates'])
+
+gulp.task('build',['html','js', 'css', 'sass', 'image', 'templates'])
+
+/* = 帮助提示( Help )
+ -------------------------------------------------------------- */
+gulp.task('help',function () {
+    console.log('----------------- 开发环境 -----------------');
+    console.log('gulp default	开启监测文件变化');
+    console.log('gulp html		HTML处理');
+    console.log('gulp sass		样式处理');
+    console.log('gulp js		JS文件压缩');
+    console.log('gulp image		图片压缩');
+    //console.log('gulp concat		文件合并');
+    console.log('---------------- 发布环境 -----------------');
+    console.log('gulp build		打包发布');
+    console.log('gulp clean		清理文件');
+    console.log('---------------------------------------------');
+});
+
